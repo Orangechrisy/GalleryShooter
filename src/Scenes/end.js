76e030659cb4c30b1c6b860ed3163fc95a6cb61e
Orangechrisy <chris@till.com>
2024-05-08 22:34:16 -0700

    put the too in the wrong place :(

--- src/Scenes/end.js
+++ src/Scenes/end.js
@@ -8,8 +8,8 @@ class end extends Phaser.Scene {
     }
     create() {
         this.add.bitmapText(game.config.width/2, game.config.height/2, "publicPixel", "GAME END", 50).setOrigin(0.5);
-        this.add.bitmapText(game.config.width/2, 3 * game.config.height/4, "publicPixel", "Press T to go back to the title screen too", 16).setOrigin(0.5);
-        this.add.bitmapText(980, 580, "publicPixel", "pretend this is a good end screen", 8).setOrigin(1);
+        this.add.bitmapText(game.config.width/2, 3 * game.config.height/4, "publicPixel", "Press T to go back to the title screen", 16).setOrigin(0.5);
+        this.add.bitmapText(980, 580, "publicPixel", "pretend this is a good end screen too", 8).setOrigin(1);
 
         this.start = this.input.keyboard.addKey("T");
     }
